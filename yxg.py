9191ad3e6fbae7c67063c11cdf5219acbb4bb66b
nikfilippas@gmail.com <nikfilippas@gmail.com>
2018-10-20 23:23:20 +0100

    todotodotodo

---
+++ yxg.py
@@ -0,0 +1,51 @@
+"""
+#TODO: Replace function descriptions with ones from workstation.
+#TODO: Check workstation imports.
+
+#TODO: Write up Battaglia....................................................OK
+#TODO: Plot Arnaud(aP+aPP)/Arnaud(aPP) for different masses (loglog).........OK
+
+#TODO: Class with profiles and power spectra.
+#TODO: Separate variables in Arnaud.
+#TODO: NFW profile
+"""
+
+
+
+
+
+############################
+
+
+#k = np.linspace(0, 10, 100)
+#Pk = np.zeros_like(k)
+#for i, vec in enumerate(k):
+#    Pk[i] = integrate.quad(Arnaud, 0, np.inf)
+
+
+
+#plt.loglog(x, P)
+#plt.xlabel("$R/R_{500}$", fontsize=14)
+#plt.ylabel("Pressure, $\mathrm{[ keV\ cm^{-3} ]}$", fontsize=14)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
