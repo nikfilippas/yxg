1a9ce62bb19b0f571c7dc91cf2c94e1afbdc778b
Nick Koukoufilippas <koukoufilippasn@aslxdt35.nat.physics.ox.ac.uk>
2018-10-23 13:53:26 +0100

    R_Delta now accepts Msol, outputs Mpc

--- cosmotools.py
+++ cosmotools.py
@@ -4,10 +4,13 @@ retrieval and data analysis.
 """
 
 import numpy as np
+from astropy import units as u
 from astropy.cosmology import Planck15 as cosmo
 
 
 
+Msol = u.M_sun.to(u.kg)  # [Msol/kg] conversion
+Mpc = 1e6*u.parsec.to(u.m)  # [Mpc/m] conversion
 h = lambda z: cosmo.H(z).value/cosmo.H0.value  # reduced Hubble's constant
 rho_cr_SI = lambda z: 1e3*cosmo.critical_density(z).value  # rho_cr at z (SI)
 
@@ -15,9 +18,12 @@ rho_cr_SI = lambda z: 1e3*cosmo.critical_density(z).value  # rho_cr at z (SI)
 def R_Delta(Delta, M, z):
     """Computes the radius at which the enclosed halo mass equals ``Delta``
     times the critical density of the Universe at a given redshift ``z``.
-    Mass should be given in ``SI`` units.
+    Mass should be given in solar masses.
+
+    - Output units in ``Mpc``.
     """
+    M *= Msol  # convert to solar masses
     rho_cr = rho_cr_SI(z)  # rho_crit at z
 
     R = (3*M / (4*np.pi*Delta*rho_cr))**(1/3)
-    return R
+    return R/Mpc
