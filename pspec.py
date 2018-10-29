95684f5e8c7eda0c13d982aa52d5f788bfab5288
Nick Koukoufilippas <koukoufilippasn@aslxdt35.nat.physics.ox.ac.uk>
2018-10-29 21:54:38 +0000

    implemented CCL; improved docstrings; replaced z with a; R_Delta fix

--- pspec.py
+++ pspec.py
@@ -1,26 +1,46 @@
 """
+- implemented CCL
+- improved docstrings
+- replaced redshift with scale factor in line with CCL
+- replaced R_Delta calculations with custom function using CCL
 """
 
 
 import numpy as np
 from scipy.integrate import quad, simps
 from scipy.interpolate import interp1d
+from pyccl.massfunction import massfunc, massfunc_m2r, halo_bias
+from pyccl.power import linear_matter_power as P_lin
+from pyccl.background import h_over_h0 as h
 
-from cosmotools import h, R_Delta, scale_factor  # TODO: replace with CCL
+from cosmotools import R_Delta
 
 
 
 class Profile(object):
-    """This class instantiates thermal pressure profiles in haloes.
-    It can compute the radial profiles and their respective Fourier transforms.
-    User has to input some scaled radius, ``r/R``, where ``R`` is characterised
-    by an overdensity parameter ``Delta``.
+    """
+    Calculate the thermal pressure profile of a halo and its fourier transform.
 
-    Each profile has a different default value of ``Delta``, so the user simply
-    has to create a ``Profile`` object with argument a string containing the
-    profile of choice.
 
-    - Note: User should input the ``M500`` mass in solar masses.
+    Parameters
+    ----------
+    cosmo : (:obj: `Cosmology`) object
+        Cosmological parameters.
+    profile : str
+        Specifies the profile to use. Implemented profiles are 'arnaud',
+        'battaglia'.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import pyccl as ccl
+    >>> cosmo = ccl.Cosmology(Omega_c=0.27, Omega_b=0.045, h=0.67, A_s=2.1e-9,
+                              n_s=0.96)
+    >>> p1 = Profile(cosmo, profile="arnaud")
+    >>> x = np.linspace(1e-3, 2, 100)  # R/R_Δ
+    >>> # radial profile is the product of the normalisation and the form factor
+    >>> radial_profile = p1.norm(1e+14, 0.5) * p1.form_factor(x)
+
     """
     def __init__(self, cosmo, profile=None):
         # Input handling
@@ -32,11 +52,16 @@ class Profile(object):
         except KeyError:
             print("Profile does not exist or has not been implemented.")
 
-        self.cosmo = cosmo  # TODO: implement in subclass
-        self.fourier_interp = self.integ_interp()
+        self.cosmo = cosmo
+        self._fourier_interp = self._integ_interp()
 
 
-    def integ_interp(self):
+    def cosmo(self):  # FIXME: maybe delete this?
+        """Workaround to call the Cosmology object from subclasses."""
+        return self.cosmo
+
+
+    def _integ_interp(self):
         """Computes the integral of the power spectrum at different points and
         returns an interpolating function connecting these points.
         """
@@ -52,7 +77,7 @@ class Profile(object):
         q_arr = np.logspace(np.log10(qmin), np.log10(qmax), qpoints)
         f_arr = [quad(integrand, 0, np.inf, args=q, limit=100)[0] for q in q_arr]
 
-        F = interp1d(np.log(q_arr), np.array(f_arr), kind="cubic", fill_value=0)
+        F = interp1d(np.log(q_arr), np.array(f_arr), kind="cubic", fill_value=0)  # TODO: check this
         return F
 
 
@@ -62,17 +87,17 @@ class Profile(object):
         return self.profile.form_factor(x)
 
 
-    def norm(self, M, z):
+    def norm(self, M, a):
         """Computes the normalisation factor of the profile.
         """
-        return self.profile.P0(M, z)
+        return self.profile.P0(M, a)
 
 
-    def fourier_profile(self, k, M, z):
+    def fourier_profile(self, k, M, a):
         """Computes the Fourier transform of the full profile.
         """
-        R = R_Delta(self.profile.Delta, M, z)
-        F = self.norm(M, z) * self.fourier_interp(np.exp(k*R)) * R**3
+        R = R_Delta(self.cosmo, M, self.profile.Delta)  # R_Δ [Mpc]
+        F = self.norm(M, a) * self._fourier_interp(np.exp(k*R)) * R**3
         return F
 
 
@@ -80,19 +105,20 @@ class Profile(object):
 class Arnaud(Profile):
 
     def __init__(self):
+#        super.cosmo  # FIXME: implement this
         self.Delta = 500  # reference overdensity (Arnaud et al.)
 
 
-    def P0(self, M, z):
+    def P0(self, M, a):
         """Yields the normalisation factor of the Arnaud profile.
         """
         aP = 0.12  # Arnaud et al.
-#        h70 = self.cosmo["h"].value/0.7
-        h70 = h(0)/0.7  # FIXME: replace with CCL (see above)
+        h70 = super.cosmo()["h"].value/0.7
         P0 = 6.41 # reference pressure
 
         K = 1.65*h70**2*P0 * (h70/3e14)**(2/3+aP)  # prefactor
-        Pz = h(z)**(8/3)  # redshift dependence  # FIXME: replace with CCL
+
+        Pz = h(self.cosmo, a)**(8/3)  # scale factor (redshift) dependence
         PM = M**(2/3+aP)  # mass dependence
         P = K*Pz*PM
         return P
@@ -122,7 +148,7 @@ class Battaglia(Profile):
 
 
 
-def power_spectrum(cosmo, k_arr, z, prof1=None, prof2=None):
+def power_spectrum(cosmo, k_arr, a, prof1=None, prof2=None):
     """Uses the halo model prescription for the 3D power spectrum to compute
     the cross power spectrum of two profiles, ``prof1`` and ``prof2``.
 
@@ -135,15 +161,14 @@ def power_spectrum(cosmo, k_arr, z, prof1=None, prof2=None):
 
     # Set up integration bounds
     logMmin, logMmax = 10, 16  # log of min and max halo mass [Msol]
-    mpoints = 10  # number of integration points
+    mpoints = 1e2  # number of integration points
 
     M_arr = np.logspace(logMmin, logMmax, mpoints)
     I = np.zeros((len(k_arr), len(M_arr)))  # initialize
     for m, M in enumerate(M_arr):
-        U = p1.fourier_profile(k_arr, M, z)
-        V = p2.fourier_profile(k_arr, M, z)
-#        mfunc = ccl.massfunc(cosmo, M, scale_factor(z), p1.Delta)
-        mfunc = 1e-4  # FIXME: replace with CCL (see above)
+        U = p1.fourier_profile(k_arr, M, a)
+        V = p2.fourier_profile(k_arr, M, a)
+        mfunc = massfunc(cosmo, M, a, p1.Delta)  # mass function
 
         I[:, m] = mfunc*U*V
 
