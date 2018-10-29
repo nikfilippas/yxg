5a65f39843458ac261b3f267761ebd68e0eb5df7
Nick Koukoufilippas <koukoufilippasn@aslxdt35.nat.physics.ox.ac.uk>
2018-10-29 21:53:00 +0000

    nothing much

--- temp.py
+++ temp.py
@@ -4,7 +4,7 @@ This script calculates the 1h- and 2h- halo contribution of any two profiles.
 
 
 import numpy as np
-from scipy.integrate import quad, simps
+from scipy.integrate import quad
 from scipy.interpolate import interp1d
 import pyccl as ccl
 
@@ -25,14 +25,3 @@ def integ_interp(integrand, a, b, xmin, xmax,  npoints=100, *args):
 
 # Cosmology Definition
 cosmo = ccl.Cosmology(Omega_c=0.27, Omega_b=0.045, h=0.67, A_s=2.1e-9, n_s=0.96)
-
-
-
-
-
-
-
-
-
-
-
