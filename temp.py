f4b1cb545dc6bd7031be0291e1b8d1df6c00dca3
Nick Koukoufilippas <koukoufilippasn@aslxdt35.nat.physics.ox.ac.uk>
2018-10-31 17:04:22 +0000

    new temp; scrapped old func definitions

--- temp.py
+++ temp.py
@@ -1,26 +1,9 @@
 """
-This script calculates the 1h- and 2h- halo contribution of any two profiles.
+temp file for calcs and unstable test code
 """
 
 
-import numpy as np
-from scipy.integrate import quad
-from scipy.interpolate import interp1d
 import pyccl as ccl
-
-
-def integ_interp(integrand, a, b, xmin, xmax,  npoints=100, *args):
-    """Integrates an expression with integration bounds ``a`` and ``b``
-    recursively by varying a parameter ``x`` within that expression between
-    ``xmin`` and ``xmax``.
-
-    Interpolates the resulting x versus int(expression|x) plot and returns
-    an interpolating function.
-    """
-    x_arr = np.logspace(xmin, xmax, npoints)
-    f_arr = np.array([ quad(integrand, a, b, args=args) for x in x_arr ])
-    F = interp1d(x_arr, f_arr, fill_value=0)
-    return F
 
 
 # Cosmology Definition
