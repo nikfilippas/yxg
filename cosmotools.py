226ee9a1a66ebf62aa032f4e62a53363b0afe587
David Alonso <dam.phys@gmail.com>
2018-12-03 11:01:22 +0000

    added new c(M) relation for Delta=500

--- cosmotools.py
+++ cosmotools.py
@@ -8,6 +8,36 @@ import numpy as np
 from scipy.interpolate import interp1d
 import pyccl as ccl
 
+def concentration_duffy(halo_mass,a,is_D500=False) :
+    """
+    Concentration-Mass relation from 0804.2486
+    Extended to Delta=500 (critical density)
+
+    .. note:: Returns A*(halo_mass/M_pivot)**B/a**C
+              where (A,B,C) depend on the mass definition
+              and M_pivot=1E12 M_sun/h
+
+    Arguments
+    ---------
+    halo_mass : float or array_like
+        Halo mass [Msun].
+    a : float
+        Scale factor
+    is_D500 : boolean
+        If `True`, return the extension of the original Duffy et al. relation for Delta=500.
+
+    Returns
+    -------
+    float or array_like : The halo concentration.
+    """
+    m_pivot=2.78164E12 #Pivot mass in M_sun
+
+    if is_D500 :
+        A_Delta=3.67; B_Delta=-0.0903; C_Delta=-0.51;
+    else : #2nd row in Table 1 of Duffy et al. 2008
+        A_Delta=5.71; B_Delta=-0.084; C_Delta=-0.47;
+    
+    return A_Delta*(halo_mass/m_pivot)**B_Delta/a**C_Delta
 
 def R_Delta(cosmo, halo_mass, a, Delta=200, is_matter=False) :
     """
