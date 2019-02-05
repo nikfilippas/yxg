added correct HOD profile variance

likelihood/likelihood.py
@@ -21,22 +21,19 @@ def func(args):
                  (0.1 <= kwargs["fc"] <= 1.0)
 
     # Piecewise probability handling
-#    if not prior_test:
-#        lnprob = -np.inf
-#    else:
-#        Cl = pspec.ang_power_spectrum(cosmo, ells, prof, prof, is_zlog=False, **kwargs)
-#        if Cl is None:
-#            lnprob = -np.inf
-#        else :
-#            lnprob = -0.5*np.dot(cells-Cl, np.dot(I, cells-Cl))
+    if not prior_test:
+        lnprob = -np.inf
+    else:
+        Cl = pspec.ang_power_spectrum(cosmo, ells, prof, prof,
+                                      zrange=(0.001,0.3),zpoints=64, is_zlog=False,
+                                      **kwargs)
+        if Cl is None:
+            lnprob = -np.inf
+        else :
+            lnprob = -0.5*np.dot(cells-Cl, np.dot(I, cells-Cl))
 
-    Cl = pspec.ang_power_spectrum(cosmo, ells, prof, prof, is_zlog=False, **kwargs)
-    lnprob = -0.5*np.dot(cells-Cl, np.dot(I, cells-Cl))
-
-#    print(args)  # output trial parameter values
+    print(args,-2*lnprob,len(ells))  # output trial parameter values
     return -lnprob
-
-
 
 Neval = 1  # display number of evaluations
 def callbackf(X):
@@ -45,8 +42,6 @@ def callbackf(X):
           {4: 3.6f}   {5: 3.6f}   {6: 3.6f}".format(Neval, X[0], X[1], X[2],
                                                            X[3], X[4], X[5]))
     Neval += 1
-
-
 
 ## DATA ##
 dcl = np.load("../analysis/out_ns512_linlog/cl_2mpz_2mpz.npz")
@@ -74,32 +69,28 @@ cosmo = ccl.Cosmology(Omega_c=0.27, Omega_b=0.045, h=0.67, sigma8=0.8, n_s=0.96)
 nz = "../analysis/data/dndz/2MPZ_bin1.txt"
 prof = profile2D.HOD(nz_file=nz)
 
-kwargs = {"Mmin"      : 12,
-          "M0"        : 12.2,
-          "M1"        : 13.65,
-          "sigma_lnM" : 0.5,
-          "alpha"     : 1.0,
-          "fc"        : 0.8}
-Cl = pspec.ang_power_spectrum(cosmo, ells, prof, prof, is_zlog=False, **kwargs)
+kwargs={"Mmin"      : 12.3,
+        "M0"        : 12.5,
+        "M1"        : 13.65,
+        "sigma_lnM" : 0.5,
+        "alpha"     : 1.1,
+        "fc"        : 0.8}
+Cl = pspec.ang_power_spectrum(cosmo, ells, prof, prof,
+                              zrange=(0.001,0.3),zpoints=64, is_zlog=False,**kwargs)
 I = np.linalg.inv(covar)  # inverse covariance
 
 
-p0 = [12, 12.2, 13.65, 0.5, 1.0, 0.8]
+p0 = [12.3, 12.5, 13.65, 0.5, 1.1, 0.8]
 res = minimize(func, p0, method="Powell", callback=callbackf)#, options={"maxiter" : 6000})
 
-
 ### PLOTS ##
-#import matplotlib.pyplot as plt
-#from matplotlib.colors import SymLogNorm
-#from pylab import cm
-## PLOT 1
-#plt.figure()
-#plt.errorbar(ells, cells, yerr=err_ell, fmt='rs', ms=5)
-#plt.loglog()
-#plt.xlabel('$\\ell$',fontsize=15)
-#plt.ylabel('$C_\\ell$',fontsize=15)
-## PLOT 2
-#plt.loglog(ells, Cl)
-## PLOT 3
-#P = plt.subplots(1, 2)
-#[x.imshow(X.T, cmap=cm.viridis, norm=SymLogNorm(linthresh=1e-17, vmin=X.min(), vmax=X.max())) for x, X in zip(P[1], [covar, I])]
+import matplotlib.pyplot as plt
+from matplotlib.colors import SymLogNorm
+from pylab import cm
+plt.figure()
+plt.errorbar(ells, cells, yerr=err_ell, fmt='r.', ms=5)
+plt.loglog(ells, Cl,'k-')
+plt.loglog()
+plt.xlabel('$\\ell$',fontsize=15)
+plt.ylabel('$C_\\ell$',fontsize=15)
+plt.show()

likelihood/profile2D.py
@@ -48,22 +48,22 @@ class Arnaud(object):
         return 1
 
 
-#    def norm(self, cosmo, M, a, b=0.4):
-#        """Computes the normalisation factor of the Arnaud profile.
-#
-#        .. note:: Normalisation factor is given in units of ``eV/cm^3``. \
-#        (Arnaud et al., 2009)
-#        """
-#        aP = 0.12  # Arnaud et al.
-#        h70 = cosmo["h"]/0.7
-#        P0 = 6.41 # reference pressure
-#
-#        K = 1.65*h70**2*P0 * (h70/3e14)**(2/3+aP)  # prefactor
-#
-#        Pz = ccl.h_over_h0(cosmo, a)**(8/3)  # scale factor (z) dependence
-#        PM = (M*(1-b))**(2/3+aP)             # mass dependence
-#        P = K*Pz*PM
-#        return P
+    def norm(self, cosmo, M, a, b=0.4):
+        """Computes the normalisation factor of the Arnaud profile.
+
+        .. note:: Normalisation factor is given in units of ``eV/cm^3``. \
+        (Arnaud et al., 2009)
+        """
+        aP = 0.12  # Arnaud et al.
+        h70 = cosmo["h"]/0.7
+        P0 = 6.41 # reference pressure
+
+        K = 1.65*h70**2*P0 * (h70/3e14)**(2/3+aP)  # prefactor
+
+        Pz = ccl.h_over_h0(cosmo, a)**(8/3)  # scale factor (z) dependence
+        PM = (M*(1-b))**(2/3+aP)             # mass dependence
+        P = K*Pz*PM
+        return P
 
 
     def form_factor(self, x):
@@ -119,7 +119,7 @@ class Arnaud(object):
         return F
 
 
-    def fourier_profile(self, cosmo, k, M, a, **kwargs):
+    def fourier_profiles(self, cosmo, k, M, a, **kwargs):
         """Computes the Fourier transform of the Arnaud profile.
 
         .. note:: Output units are ``[norm] Mpc^3``
@@ -127,8 +127,8 @@ class Arnaud(object):
         b = kwargs["b_hydro"]  # hydrostatic bias
         R = ct.R_Delta(cosmo, M, a, self.Delta) / a  # R_Delta*(1+z) [Mpc]
         F = self.norm(cosmo, M, a, b) * self._fourier_interp(np.log10(k*R))
-        return 4*np.pi * R**3 * F
-
+        F *= 4*np.pi*R**3
+        return F, F**2
 
 
 class NFW(object):
@@ -166,7 +166,7 @@ class NFW(object):
         return P1*P2
 
 
-    def fourier_profile(self, cosmo, k, M, a, **kwargs):
+    def fourier_profiles(self, cosmo, k, M, a, **kwargs):
         """Computes the Fourier transform of the Navarro-Frenk-White profile."""
         c = ct.concentration_duffy(M, a, is_D500=True)
 
@@ -180,9 +180,7 @@ class NFW(object):
         P3 = np.sin(c*x)/((1+c)*x)
 
         F = P1*(P2-P3)
-        return F
-
-
+        return F, F**2
 
 class HOD(object):
     """Calculates a Halo Occupation Distribution profile quantity of a halo."""
@@ -219,7 +217,9 @@ class HOD(object):
         delta_matter = self.Delta/ccl.omega_x(cosmo, a, "matter")  # CCL uses Dm
         mfunc = ccl.massfunc(cosmo, M_arr, a, delta_matter)        # mass function
         Nc = 0.5 * (1 + erf((np.log10(M_arr/Mmin))/sigma_lnM))     # centrals
-        Ns = np.heaviside(M_arr-M0, 0.5) * ((M_arr-M0)/M1)**alpha  # satellites
+        f1=lambda x :np.zeros_like(x)
+        f2=lambda x :((x-M0)/M1)**alpha
+        Ns=np.piecewise(M_arr,[M_arr<=M0,M_arr>M0],[f1,f2]) # satellites
 
         dng = mfunc*Nc*(fc+Ns)  # integrand
 
@@ -227,7 +227,7 @@ class HOD(object):
         return ng
 
 
-    def fourier_profile(self, cosmo, k, M, a, **kwargs):
+    def fourier_profiles(self, cosmo, k, M, a, **kwargs):
         """Computes the Fourier transform of the Halo Occupation Distribution.
         Default parameter values from Krause & Eifler (2014).
         """
@@ -241,8 +241,12 @@ class HOD(object):
 
         # HOD Model
         Nc = 0.5 * (1 + erf((np.log10(M/Mmin))/sigma_lnM))  # centrals
-        Ns = np.heaviside(M-M0, 0.5) * ((M-M0)/M1)**alpha   # satellites
+        if M<=M0 :
+            Ns=0
+        else :
+            Ns=((M-M0)/M1)**alpha   # satellites
 
-        H = NFW().fourier_profile(cosmo, k, M, a)
+        H,_ = NFW().fourier_profiles(cosmo, k, M, a)
 
-        return Nc * (fc + Ns*H)
+        return Nc * (fc + Ns*H), Nc * (2*fc*Ns*H + (Ns*H)**2)
+    

likelihood/pspec.py
@@ -60,10 +60,14 @@ def power_spectrum(cosmo, k_arr, a, p1, p2,
     # initialise integrands
     I1h, I2h_1, I2h_2 = [np.zeros((len(k_arr), len(M_arr)))  for i in range(3)]
     for m, M in enumerate(M_arr):
-        U = p1.fourier_profile(cosmo, k_arr, M, a, **kwargs)
-        V = p2.fourier_profile(cosmo, k_arr, M, a, **kwargs)
+        U,UU = p1.fourier_profiles(cosmo, k_arr, M, a, **kwargs)
+        if p1==p2 :
+            V=U; UV=UU
+        else :
+            V,VV = p2.fourier_profiles(cosmo, k_arr, M, a, **kwargs)
+            UV=U*V
 
-        I1h[:, m] = mfunc[m]*U*V
+        I1h[:, m] = mfunc[m]*UV
         I2h_1[:, m] = bh[m]*mfunc[m]*U
         I2h_2[:, m] = bh[m]*mfunc[m]*V
 
@@ -79,12 +83,17 @@ def power_spectrum(cosmo, k_arr, a, p1, p2,
     n0_1h = (rhoM - np.sum(mfunc*M_arr) * dlM) / M_arr[0]
     n0_2h = (rhoM - np.sum(mfunc*bh*M_arr) * dlM) / M_arr[0]
 
-    prof1_0 = p1.fourier_profile(cosmo, k_arr, M_arr[0], a, **kwargs)
-    prof2_0 = p2.fourier_profile(cosmo, k_arr, M_arr[0], a, **kwargs)
+    prof1_0,prof1_02 = p1.fourier_profiles(cosmo, k_arr, M_arr[0], a, **kwargs)
+    if p1==p2 :
+        prof2_0=prof1_0
+        prof12_0=prof1_02
+    else :
+        prof2_0,prof2_02 = p2.fourier_profiles(cosmo, k_arr, M_arr[0], a, **kwargs)
+        prof12_0=prof1_0*prof2_0
 
     b2h_1 += n0_2h*prof1_0
     b2h_2 += n0_2h*prof2_0
-    P1h += n0_1h*prof1_0*prof2_0
+    P1h += n0_1h*prof12_0
 
     F = (include_1h*P1h + include_2h*(Pl*b2h_1*b2h_2)) / (Unorm*Vnorm)
     return F