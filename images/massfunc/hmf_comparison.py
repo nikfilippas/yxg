db490ea417305b306bdbc0a5d3d8b67688cb5bb1
Nick Koukoufilippas <nick.koukoufilippas@physics.ox.ac.uk>
2019-05-17 22:23:36 +0100

    CCL/HMF comparison

--- images/massfunc/hmf_comparison.py
+++ images/massfunc/hmf_comparison.py
@@ -12,7 +12,7 @@ cosmo = ccl.Cosmology(Omega_c=0.2678,
                       h=0.6704,
                       sigma8=0.8347,
                       n_s=0.9619)
-
+h = cosmo["h"]
 
 datadir = "mfuncs/"
 num1 = ["08", "10"]
@@ -27,12 +27,14 @@ for mfname in itertools.product(*(num1, num2)):
 for f in os.listdir(datadir):
     if f[-3:] == "txt":
         data = np.loadtxt(datadir + f, skiprows=12, usecols=(0, 7))
+        x, y = data.T
+
         z = int(f[-8: -5]) / 100
         massfunc = ccl.massfunc(cosmo, M, 1/(1+z), overdensity=500)
 
-        plt.loglog(data[:, 0], data[:, 1], "b-", lw=3)
+        plt.loglog(x*h, y/h**3, "b-", lw=3)
         plt.loglog(M, massfunc, "r-", lw=3)
-        plt.xlabel("M")
-        plt.ylabel("dn/d(log10(M))")
-        plt.savefig(datadir + "plots" + f[8:-5] + ".pdf")
+        plt.xlabel("$M/h$", fontsize=16)
+        plt.ylabel(r"$\frac{dn}{d \ \log_{10}(M)} \ h^3$", fontsize=16)
+        plt.savefig(datadir + "plots" + f[8:-5] + ".pdf", bbox_inches="tight")
         plt.close()
