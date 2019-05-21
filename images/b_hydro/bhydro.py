aa213eeacc1cdf372e36fe0f3e5d66686247dd5c
Nick Koukoufilippas <nick.koukoufilippas@physics.ox.ac.uk>
2019-05-22 00:18:15 +0100

    final b_hydro plot

--- images/b_hydro/bhydro.py
+++ images/b_hydro/bhydro.py
@@ -9,9 +9,9 @@ class AnyObjectHandler(HandlerBase):
     def create_artists(self, legend, orig_handle,
                        x0, y0, width, height, fontsize, trans):
         l1 = plt.Line2D([x0,y0+width], [0.7*height,0.7*height],
-                                                linestyle="--", color="brown")
+                        lw=3, linestyle="-", color="cadetblue")
         l2 = plt.Line2D([x0,y0+width], [0.3*height,0.3*height],
-                                                linestyle="--", color="grey")
+                        lw=3, linestyle="-", color="grey")
         return [l1, l2]
 
 
@@ -33,34 +33,33 @@ dndz = np.load("dndz.npy")
 z = fiducial[0]  # probed redshifts
 
 
-data = [fiducial, dndz, masked, mfunc, ynilc]
-colours = ["k", "salmon", "royalblue", "forestgreen", "deeppink"]
+data = [fiducial, ynilc, mfunc, dndz, masked]
+colours = ["k", "brown", "darkorange", "orangered", "y"]
 fmts = ["o"]*len(data)
-lbls = ["fiducial", "dndz", "masked", "mfunc", "ynilc"]
+lbls = ["fiducial", "y-nilc", "Tinker10", "dn/dz", "high mass mask"]
 
 
 fig, ax = plt.subplots(figsize=(10, 7))
 
-ax.axhline(0.58, ls="--", color="grey")
+ax.axhline(0.58, ls=":", color="grey")
 ax.axhspan(0.58-0.06, 0.58+0.06, color="grey", alpha=0.3)
-ax.axhline(0.72, ls="--", color="brown")
-ax.axhspan(0.72-0.10, 0.72+0.10, color="brown", alpha=0.3)
+ax.axhline(0.72, ls=":", color="cadetblue")
+ax.axhspan(0.72-0.10, 0.72+0.10, color="cadetblue", alpha=0.3)
 
-ax.annotate(r"\textbf{CMB + cluster counts}", (0.055, 0.585), fontsize=12)
-ax.annotate(r"\textbf{CMB lensing}", (0.055, 0.725), fontsize=12)
+ax.annotate("CMB + cluster counts",
+            (0.055, 0.585), fontsize=9.5, fontweight="bold")
+ax.annotate("CMB lensing", (0.055, 0.725),
+            fontsize=9.5, fontweight="bold")
 
 
 for i, (a, c, fmt, lbl) in enumerate(zip(data, colours, fmts, lbls)):
-    plotfunc(ax, a+0.005*i, xerr=None, fmt=fmt, color=c, label=lbl)
+    plotfunc(ax, a+0.003*i, xerr=None, fmt=fmt, color=c, label=lbl)
 
 
 handles, labels = ax.get_legend_handles_labels()
 ax.legend([object]+handles, ["Planck18"]+labels,
           handler_map={object: AnyObjectHandler()},
-          loc="lower center", fontsize=14, ncol=2)
-
-plt.rc("text", usetex=True)
-plt.rc("font", family="serif")
+          loc="lower center", fontsize=14, ncol=3)
 
 ax.set_xlabel(r"$z$", fontsize=16)
 ax.set_ylabel(r"$1-b_H$", fontsize=16)
