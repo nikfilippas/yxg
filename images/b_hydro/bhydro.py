9ba575807314075cf3945a0e148b1ac678e2f1b1
Nick Koukoufilippas <nick.koukoufilippas@physics.ox.ac.uk>
2019-05-21 19:12:44 +0100

    new bhydro plot

--- images/b_hydro/bhydro.py
+++ images/b_hydro/bhydro.py
@@ -1,43 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerBase
 
-ymilca = np.load("ymilca.npy")
-ynilc = np.load("ynilc.npy")
-masked_ymilca = np.load("masked_ymilca.npy")
-masked_nilc = np.load("masked_ynilc.npy")
-mfunc_ymilca = np.load("mfunc_ymilca.npy")
-mfunc_ynilc = np.load("mfunc_ynilc.npy")
-dndz_ymilca = np.load("dndz_ymilca.npy")
-dndz_ynilc = np.load("dndz_ynilc.npy")
 
-def plotfunc(a, xerr=None, fmt=None, color=None, alpha=0, label=None):
+class AnyObjectHandler(HandlerBase):
+# Adapted from:
+# https://matplotlib.org/users/legend_guide.html#legend-handlers
+    def create_artists(self, legend, orig_handle,
+                       x0, y0, width, height, fontsize, trans):
+        l1 = plt.Line2D([x0,y0+width], [0.7*height,0.7*height],
+                                                linestyle="--", color="brown")
+        l2 = plt.Line2D([x0,y0+width], [0.3*height,0.3*height],
+                                                linestyle="--", color="grey")
+        return [l1, l2]
+
+
+def plotfunc(ax, a, xerr=None, fmt=None, color=None, label=None):
     """Plots b_hydro data with error bars."""
     x, y = a[0: 2]
     if xerr: xerr = a[2]
     yerr = a[3:]
-    plt.errorbar(x, y, xerr=xerr, yerr=yerr,
-                 fmt=fmt, c=color, alpha=alpha, label=label)
+    ax.errorbar(x, y, xerr=xerr, yerr=yerr,
+                 fmt=fmt, c=color, label=label)
 
 
-data = [ymilca, ynilc,
-        masked_ymilca, masked_nilc,
-        mfunc_ymilca, mfunc_ynilc,
-        dndz_ymilca, dndz_ynilc]
-colours = ["k", "k",
-           "salmon", "salmon",
-           "royalblue", "royalblue",
-           "forestgreen", "forestgreen"]
-fmts = ["o", "^"]*4
-alphas = [1, 0.5]*4
-lbls = ["ymilca", "ynilc",
-        "masked_ymilca", "masked_nilc",
-        "mfunc_ymilca", "mfunc_ynilc",
-        "dndz_ymilca", "dndz_ynilc"]
+fiducial = np.load("fiducial.npy")
+ynilc = np.load("ynilc.npy")
+masked = np.load("masked.npy")
+mfunc = np.load("mfunc.npy")
+dndz = np.load("dndz.npy")
 
-for a, c, fmt, alpha, lbl in zip(data, colours, fmts, alphas, lbls):
-    plotfunc(a, xerr=None, fmt=fmt, color=c, alpha=alpha, label=lbl)
+z = fiducial[0]  # probed redshifts
 
-plt.legend(loc="lower right", fontsize=10, ncol=2)
-plt.xlabel("z", fontsize=14)
-plt.ylabel(r"$\mathrm{b_{hydro}}$", fontsize=14)
+
+data = [fiducial, dndz, masked, mfunc, ynilc]
+colours = ["k", "salmon", "royalblue", "forestgreen", "deeppink"]
+fmts = ["o"]*len(data)
+lbls = ["fiducial", "dndz", "masked", "mfunc", "ynilc"]
+
+
+fig, ax = plt.subplots(figsize=(10, 7))
+
+ax.axhline(0.58, ls="--", color="grey")
+ax.axhspan(0.58-0.06, 0.58+0.06, color="grey", alpha=0.3)
+ax.axhline(0.72, ls="--", color="brown")
+ax.axhspan(0.72-0.10, 0.72+0.10, color="brown", alpha=0.3)
+
+ax.annotate(r"\textbf{CMB + cluster counts}", (0.055, 0.585), fontsize=12)
+ax.annotate(r"\textbf{CMB lensing}", (0.055, 0.725), fontsize=12)
+
+
+for i, (a, c, fmt, lbl) in enumerate(zip(data, colours, fmts, lbls)):
+    plotfunc(ax, a+0.005*i, xerr=None, fmt=fmt, color=c, label=lbl)
+
+
+handles, labels = ax.get_legend_handles_labels()
+ax.legend([object]+handles, ["Planck18"]+labels,
+          handler_map={object: AnyObjectHandler()},
+          loc="lower center", fontsize=14, ncol=2)
+
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif")
+
+ax.set_xlabel(r"$z$", fontsize=16)
+ax.set_ylabel(r"$1-b_H$", fontsize=16)
 plt.savefig("bhydro.pdf")
