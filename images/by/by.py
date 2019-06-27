b7e92d77ce188eac5d1b66a5dfb09fcba8483974
Nick Koukoufilippas <nick.koukoufilippas@physics.ox.ac.uk>
2019-06-28 00:53:15 +0100

    new by plot

--- images/by/by.py
+++ images/by/by.py
@@ -1,16 +1,19 @@
+import os
+os.chdir("../../")
 import numpy as np
+from likelihood.chanal import chan
 import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 plt.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 
-run_name = "lmin10_kmax1_tinker08_ymilca"
-data = np.load("../../output_default/"+run_name+"_bH.npy")
+fname_params = "params_wnarrow.yml"
+pars, _, chains = chan(fname_params, diff=True, error_type="hpercentile")
+bys = [bb for bb in 1e3*np.array(chains[1])]
 
-z = data[0]
-by = data[-3]
-sby = data[-2:]
+z = np.array([par["z"] for par in pars])
+by = np.array([par["by"] for par in pars]).T
 
 DESx = np.array([0.14, 0.23, 0.24, 0.39, 0.40, 0.52, 0.53, 0.67, 0.68])
 DESy = 1e-1*np.array([1.4, 1.4, 0.9, 2.4, 2.5, 3.7, 3.0, 2.5, 2.2])
@@ -23,7 +26,9 @@ green = DES[:, 1::2]
 orang = DES[:, 2::2]
 
 fig, ax = plt.subplots(figsize=(9,7))
-ax.errorbar(z, 1e3*by, 1e3*sby, fmt="o", c="royalblue", elinewidth=2, label="this work")
+ax.violinplot(bys, z, widths=0.02, showmedians=True, showextrema=False)
+ax.errorbar(z, 1e3*by[0], 1e3*by[1:],
+            fmt="o", c="royalblue", elinewidth=2, label="this work")
 
 ax.errorbar(black[0], black[1], black[2], fmt="ko", elinewidth=2,
             label="Vikram16")
@@ -38,4 +43,6 @@ ax.tick_params(labelsize="large")
 ax.set_xlabel("$z$", fontsize=17)
 ax.set_ylabel(r"$\mathrm{\langle bP_e \rangle \ \big[ eV \ cm^{-3} \big] }$", fontsize=17)
 ax.legend(loc="upper left", frameon=False, fontsize=14)
-fig.savefig("by.pdf", bbox_inches="tight")
+
+os.chdir("images/by/")
+#fig.savefig("by.pdf", bbox_inches="tight")
